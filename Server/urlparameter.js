e1d99c259f2dd6a8e380cad716cb75aa1701325b
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2018-10-17 18:58:32 +0530

    Redirect

--- Server/urlparameter.js
+++ Server/urlparameter.js
@@ -11,24 +11,31 @@ var ObjectId=require('mongodb').ObjectID;
 
 app.get('/watch',function(req,res){
 	var list_available_loc;
-	id=req.query.id;
+	id=req.query.id.toString();
 	qty=req.query.qty;
+	reg=req.query.reg;
 	console.log(id,qty);
 	client.connect("mongodb://localhost:27017/youtube",function(err,db)
 	{
 		 var video_location=db.db("youtube");
-		var query= {"_id" : ObjectId("5bc4bbc50b804a4b01065b1a")};
+		var query= {"_id" :ObjectId(id)};
 		video_location.collection("videoloc").find(query).toArray(function(err,result)
 		{
 			if(err)
 				throw err;
-			list_available_loc=result[0].location;
+			list_available_loc=result[0].reg;
 			console.log(list_available_loc,result[0].location);
 			
 			db.close();
 		});
+		var reg={};
+		var ip=bestregionalserver(reg)
+		ip=ip+"?id=",id+"&qty="+qty;
+		res.redirect(ip)
+				
 	});
-
+	
+	
 	console.log(list_available_loc);
 
 
