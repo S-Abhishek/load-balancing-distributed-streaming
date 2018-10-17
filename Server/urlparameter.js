8703ede96bd2abb1e6f76be4ff881af13c2ea550
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2018-10-17 23:36:44 +0530

    Fixed error & Extracted IPS with regionid

--- Server/urlparameter.js
+++ Server/urlparameter.js
@@ -6,7 +6,42 @@ const path=require('path')
 const client=require('mongodb').MongoClient;
 var ObjectId=require('mongodb').ObjectID;
 
-
+	client.connect("mongodb://localhost:27017/youtube",function(err,db)
+	{
+		load_data={"ip1":1}
+		threshold={"ip1":2}
+		 var video_location=db.db("youtube");
+		
+		
+		var lcq="location.India";
+		
+		console.log("here");
+		var query={"_id" :ObjectId("5bc76596121ead5cea907ea2"),[lcq]:{$exists:true}};
+		video_location.collection("videoloc").find(query,{[lcq]:1,_id:0}).toArray(function(err,result)
+		{
+			if(err)
+				throw err;
+			if(result.length==0)
+			{
+			}
+			else
+			{
+				list_available_loc=result[0].location;
+				console.log(list_available_loc);
+				//console.log(result[0].location);
+				//var key in list_available_loc
+				//var value=list_available_loc[key];
+				//console.log(key,value);
+			}
+			
+			db.close();
+		});
+		var reg={};
+		//var ip=bestregionalserver(reg)
+		//ip=ip+"?id=",id+"&qty="+qty;
+		//res.redirect(ip)
+				
+	});
 
 
 app.get('/watch',function(req,res){
@@ -17,14 +52,16 @@ app.get('/watch',function(req,res){
 	console.log(id,qty);
 	client.connect("mongodb://localhost:27017/youtube",function(err,db)
 	{
+		load_data={"ip1":1}
+		threshold={"ip1":2}
 		 var video_location=db.db("youtube");
-		var query= {"_id" :ObjectId(id)};
+		//var query= {"_id" :ObjectId(id),"Location.};
 		video_location.collection("videoloc").find(query).toArray(function(err,result)
 		{
 			if(err)
 				throw err;
-			list_available_loc=result[0].reg;
-			console.log(list_available_loc,result[0].location);
+			list_available_loc=result[0].location;
+			console.log(result[0].location);
 			
 			db.close();
 		});
