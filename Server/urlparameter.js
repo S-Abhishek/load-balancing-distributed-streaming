010b541ca5bf362d65d803340ef480d27833601b
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2018-10-17 23:58:10 +0530

    Region url parameter undefined inside mongodb connection

--- Server/urlparameter.js
+++ Server/urlparameter.js
@@ -6,16 +6,26 @@ const path=require('path')
 const client=require('mongodb').MongoClient;
 var ObjectId=require('mongodb').ObjectID;
 
+	
+
+
+app.get('/watch',function(req,res){
+	var list_available_loc;
+	id=req.query.id.toString();
+	qty=req.query.qty;
+	reg=req.query.reg.toString();
+	console.log(id,qty,reg);
 	client.connect("mongodb://localhost:27017/youtube",function(err,db)
 	{
 		load_data={"ip1":1}
 		threshold={"ip1":2}
 		 var video_location=db.db("youtube");
 		
+		console.log(id,qty,reg);
+		var lcq="location.".concat(reg);
 		
-		var lcq="location.India";
+		console.log("here",lcq);
 		
-		console.log("here");
 		var query={"_id" :ObjectId("5bc76596121ead5cea907ea2"),[lcq]:{$exists:true}};
 		video_location.collection("videoloc").find(query,{[lcq]:1,_id:0}).toArray(function(err,result)
 		{
@@ -29,8 +39,12 @@ var ObjectId=require('mongodb').ObjectID;
 				list_available_loc=result[0].location;
 				console.log(list_available_loc);
 				//console.log(result[0].location);
-				//var key in list_available_loc
-				//var value=list_available_loc[key];
+				
+				for(var key in list_available_loc)
+				{
+					for(var value in list_available_loc[key])
+						console.log(list_available_loc[key][value]);
+				}
 				//console.log(key,value);
 			}
 			
@@ -40,35 +54,6 @@ var ObjectId=require('mongodb').ObjectID;
 		//var ip=bestregionalserver(reg)
 		//ip=ip+"?id=",id+"&qty="+qty;
 		//res.redirect(ip)
-				
-	});
-
-
-app.get('/watch',function(req,res){
-	var list_available_loc;
-	id=req.query.id.toString();
-	qty=req.query.qty;
-	reg=req.query.reg;
-	console.log(id,qty);
-	client.connect("mongodb://localhost:27017/youtube",function(err,db)
-	{
-		load_data={"ip1":1}
-		threshold={"ip1":2}
-		 var video_location=db.db("youtube");
-		//var query= {"_id" :ObjectId(id),"Location.};
-		video_location.collection("videoloc").find(query).toArray(function(err,result)
-		{
-			if(err)
-				throw err;
-			list_available_loc=result[0].location;
-			console.log(result[0].location);
-			
-			db.close();
-		});
-		var reg={};
-		var ip=bestregionalserver(reg)
-		ip=ip+"?id=",id+"&qty="+qty;
-		res.redirect(ip)
 				
 	});
 	
