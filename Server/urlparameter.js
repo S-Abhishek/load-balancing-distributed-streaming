0785371622d252165dee6958cdd227aac1b34d95
tamilmaran <tamilmaran.c2016@vitstudent.ac.in>
2018-10-19 13:01:08 +0530

    Added hot update and made db connection global

--- Server/urlparameter.js
+++ Server/urlparameter.js
@@ -12,7 +12,7 @@ var ObjectId=require('mongodb').ObjectID;
 const port = 3000
 const central_server_ip = '10.0.2.13';
 const server_address = '/tmp/loads';
-
+const HOT = 3
 
 load_data = {}
 console.log(threshold_data)
@@ -38,8 +38,6 @@ loadServer.listen( server_address , () => {
     console.log('loadServer bound');
 });
 
-
-
 function findMin(list_available_loc)
 {
 	var min = 100;
@@ -47,30 +45,30 @@ function findMin(list_available_loc)
 	for(var region in list_available_loc)
 	{ 
         console.log(region)
-		for(var ip in list_available_loc[region])
-		{
-			if(load_data[list_available_loc[region][ip]] < threshold_data[list_available_loc[region][ip]]["grade_s"])
-			{
-				if(load_data[list_available_loc[region][ip]] < min)
-				{
-					min = load_data[list_available_loc[region][ip]]
-					min_ip = list_available_loc[region][ip]
 
-				}	 
-			} 
-		}
+		if(load_data[list_available_loc[region]] < threshold_data[list_available_loc[region]]["grade_s"])
+		{
+			if(load_data[list_available_loc[region]] < min)
+			{
+				min = load_data[list_available_loc[region]]
+				min_ip = list_available_loc[region]
+
+			}	 
+		} 
+		
 	}
 	return min_ip
 }
 
-app.get('/watch',function(req,res){
-    
-    client.connect("mongodb://localhost:27017/Videodb",function(err,db)
-    {
-		
+client.connect("mongodb://localhost:27017/VideoDB",function(err,db)
+{
+	console.log("Connected to DB")
+	app.get('/watch',function(req,res)
+	{
+    	console.log("Request received")
 		if( Object.keys(req.query).length < 3)
 		{
-			res.write('Error')
+			res.send('Error')
 			return;
 		}
 	
@@ -78,47 +76,71 @@ app.get('/watch',function(req,res){
    		qty=req.query.qty;
    	 	reg=req.query.reg.toString();
 
-   	  	const video_location=db.db("Videodb");
-   	 	var lcq="location.".concat(reg);
+   	  	const VideoDB=db.db("VideoDB");
    	 
-   	 	console.log("here",lcq);
-   		const vidlocs = video_location.collection("vidlocs")
-   	 	var query={"_id" :ObjectId("5bc8558e681ac8ae25b30e42"), [lcq] : {$exists : true} };
-   	 	vidlocs.find(query).project({  [lcq] : 1, _id : 0} ).toArray(function(err,result)
+   	 	console.log("here",reg);
+   		const vidlocs = VideoDB.collection("vidlocs")
+   	 	var query={"_id" :ObjectId(id), "locations" : {$exists : true} };
+   	 	vidlocs.find(query).project({ "locations" : 1, _id : 0} ).toArray(function(err,result)
    	 	{
 			
    			if(err)
    				throw err;
 			
-			console.log(result)
-			var min_ip
-			var found = false
+			console.log(query,result)
+			var min_ip = central_server_ip
 			if(result.length)
 			{
-				
-				min_ip = findMin(result[0].location)
-				if( min_ip != central_server_ip)
-					found = true
-
-			}
-
-			if(!found)	
-   		 	{
-			var lc = "location"
-            		var query1={ "_id" :ObjectId("5bc8558e681ac8ae25b30e42"), "location" : { $exists: true}  };
-                	vidlocs.find(query1).project({"location" : 1, _id : 0}).toArray(function(err,result)
-               		 {
-					console.log(result)
-					if(result.length)
-					min_ip = findMin(result[0].location)
-              		  });
-
-   		 	}
+				const locations = result[0].locations
+				if(reg in locations && load_data[locations[reg]] < threshold_data[locations[reg]] )
+				{
+					min_ip = locations[reg]
+				} 
+				else
+				{
+					min_ip = findMin(locations)
+				}
+			
 			   
-			console.log("Redirecting to " + min_ip)
-			res.redirect("http://"+min_ip+"/watch?id="+id+"&qty="+qty);
-			db.close();
-		   
+				console.log("Redirecting to " + min_ip)
+				res.redirect("http://"+min_ip+"/watch?id="+id+"&qty="+qty);
+
+				if(!(reg in locations))
+				{
+					VideoDB.collection("hotinfo").findOneAndUpdate(
+						{ "reg" : reg }, 
+						{ $inc : { ["reg_movie_infor."+id] : 1 } },
+						{
+							upsert:true,
+							returnNewDocument: true
+						},
+						function( err, result)
+						{
+							if(err)
+								throw err;
+				
+							if( result )
+							{
+								if( result["value"]["reg_movie_infor"][id] >= HOT )
+									{
+										reg_ip = result["value"]["ip"]
+										console.log("copy dude to "+reg_ip)
+										vidlocs.findOneAndUpdate(
+											{  _id : ObjectId(id) },
+											{ $set :  { ["locations."+reg] : reg_ip } },
+											{
+												upsert:true,
+											},function(err, result){
+												console.log("Updated vidloc "+err)
+											})
+									}
+							}
+						}
+					)
+				}
+			}
+			
+			//db.close();	   
    	 	});
    			 
     });
@@ -127,5 +149,8 @@ app.get('/watch',function(req,res){
 app.listen(port, central_server_ip ,function(){
     console.log('App listening')
 });
+
+
+
 
 
